8cdd9d42ed8943cd808168e8e9887da4a34e9547
sonodaryo <144551798+sonodaryo@users.noreply.github.com>
2023-09-10 14:37:14 +0000

    section4 finish!!

--- src/index.js
+++ src/index.js
@@ -216,11 +216,45 @@ const newNameArr = nameArr.map((name) => {
 })
 console.log(newNameArr);
 
+/**
+ * 三項演算子
+ */
+//ある条件？条件がtureの時：条件がfalseの時
+// const val1 = 1 > 0 ? 'trueです' : 'falseです';
+// console.log(val1);
+
+// const num = "1300";
+// const formattedNum = typeof num === 'number' ? num.toLocaleString() : '数値を入力してください';
+// console.log(formattedNum);
 
 
+// const checkSum = (num1 , num2) => {
+//     return num1 + num2 > 100 ? '100を超えています' : '許容範囲内です';
+// }
+// console.log(checkSum(50 , 40));
 
+/**
+ * 論理演算氏の本当の意味を知ろう
+ */
+// const flg1 = true;
+// const flg2 = false;
 
+// if(flg1 || flg2){
+//     console.log("1か2はtrueになります");
+// }
+// if(flg1 && flg2){
+//     console.log("1も2もtrueになります");
+// }
 
+// || は左側がfalseなら右側を返す　左側がtrueなら右側を返す
+const num = 100;
+const fee = num || "金額未設定です";
+console.log(fee);
+
+// && は左側がtrueなら右側を返す　左側がfalseなら右側を返す
+const num2 = 100;
+const fee2 = num2 && "何か設定されました";
+console.log(fee2);
 
 
 
